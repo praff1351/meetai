468d1d8d606996d82f7311781eb3038ed3ad14c2
praff1351 <prafullihoo.7777@gmail.com>
2025-12-17 20:48:18 +0530

    04: authentication ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,96 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const Home = () => {
-  return <Button>Click Me</Button>;
+  const { data: session } = authClient.useSession();
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
+  const onLogin = () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-10">
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onSubmit}>Click me</Button>
+      </div>
+
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/lib/auth-client.js
@@ -0,0 +1,4 @@
+// the client side library helps us interact with the auth server
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({});
